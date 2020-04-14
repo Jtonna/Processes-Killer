d16bcbc88548df9618a4bba956de95c8a6ae87ba
Jacob Tonna <34466548+Jtonna@users.noreply.github.com>
2020-04-14 11:31:13 -0700

    added CMD commands for use later

--- Process Killer/cmd_commands.py
+++ Process Killer/cmd_commands.py
@@ -2,3 +2,8 @@
 Process Name's (caption), File Location (commandline) & PID's (processid)
 """
 get_proc = "WMIC PROCESS GET caption, commandline, processid"
+
+"""This command uses taskkill to kill tasks, task kill has several parameters even Admin kill, which could be usefull
+https://docs.microsoft.com/en-us/windows-server/administration/windows-commands/taskkill
+"""
+kill_proc = "taskkill /F /PID "
