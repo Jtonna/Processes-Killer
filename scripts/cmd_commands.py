a030cf6da709fa02863f06796fdf60606669191f
Jacob Tonna <34466548+Jtonna@users.noreply.github.com>
2020-04-14 23:22:52 -0700

    scrapped architecture

--- scripts/cmd_commands.py
+++
@@ -1,9 +0,0 @@
-"""Running this command in an un-elevanted command prompt will give you a readout of
-Process Name's (caption), File Location (commandline) & PID's (processid)
-"""
-get_proc = "WMIC PROCESS GET caption, commandline, processid"
-
-"""This command uses taskkill to kill tasks, task kill has several parameters even Admin kill, which could be usefull
-https://docs.microsoft.com/en-us/windows-server/administration/windows-commands/taskkill
-"""
-kill_proc = "taskkill /F /PID "
