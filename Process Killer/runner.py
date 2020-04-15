3a0fc72a1231d4e57c10c5408cf14c1d14bf414a
Jacob Tonna <34466548+Jtonna@users.noreply.github.com>
2020-04-15 12:24:10 -0700

    Updated runner to launch the gui

--- Process Killer/runner.py
+++ Process Killer/runner.py
@@ -1,9 +1,3 @@
 from gui import ui
 
-root = ui.tk.Tk()
-app = ui.Application(master=root)
-root.iconbitmap("gui/img/app_icon.ico")
-app.master.title("Application Killer")
-app.master.minsize(400,400)
-app.master.maxsize(400,400)
-app.mainloop()
+ui.LaunchApp()
