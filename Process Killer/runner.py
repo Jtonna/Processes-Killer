3dac4a0879a3b12fc67636452416ebe08883247d
Jacob Tonna <34466548+Jtonna@users.noreply.github.com>
2020-04-15 01:22:01 -0700

    Working on settings for the ui window

--- Process Killer/runner.py
+++ Process Killer/runner.py
@@ -2,4 +2,7 @@ from gui import ui
 
 root = ui.tk.Tk()
 app = ui.Application(master=root)
+app.master.title("Application Killer")
+app.master.minsize(400,400)
+app.master.maxsize(400,400)
 app.mainloop()
