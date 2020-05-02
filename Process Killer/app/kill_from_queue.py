47a7550cd962d7fbff865eebf1946dcd084e3d6c
Jacob Tonna <34466548+Jtonna@users.noreply.github.com>
2020-05-02 15:09:20 -0700

    Added comments & cleaned up the code

--- Process Killer/app/kill_from_queue.py
+++ Process Killer/app/kill_from_queue.py
@@ -1,3 +1,14 @@
+""" This Module is used to kill processes by its PROCESS ID on a Windows Machine
+    Once killer () is called, while the queue has information in it will 
+        - Dequeue the bucket
+        - Use Popen to spawn a new Command Prompt
+        - Use 'taskkill' to kill the process by PID
+    
+    !NOTE:
+    Since the application gets compiled with Pyinstallers --uac-admin & --uac-uiaccess command
+    this shell is an elevated command promt; this means that
+        - This shell can kill ANY application
+        - When using Remote Desktop, the shell will still work"""
 import subprocess
 from .app_state import state
 
@@ -6,16 +17,4 @@ def killer():
 
         item = state.remove_from_queue()
         command = "taskkill /F /PID "+item['pid']
-        # kill_task = subprocess.run(command, capture_output=True, shell=True)
-        # bytesdata = kill_task.stderr
-        # ugly_string = "".join(map(chr, bytesdata))
-        # err_no_access = "could not be terminated."
-
-        # if err_no_access in ugly_string:
-        #     ele_command = "ele taskkill /F /PID "+item['pid']
-        #     subprocess.run(command, shell=True)
-        #     print("process killed with admin")
-        # else:
-        #     print("process was killed regularly")
-
         subprocess.Popen(command, shell=True)
