3f9d3569d8d33839e9b4f49592487175e5ea571b
Jacob Tonna <34466548+Jtonna@users.noreply.github.com>
2020-05-03 15:18:06 -0700

    Module declaration & pep8 formatting

--- Process Killer/app/app_state.py
+++ Process Killer/app/app_state.py
@@ -1,3 +1,7 @@
+""" This module/class contains the application 'state', 
+    such as all of the data created by running processes
+    and what is currently happening in the application """
+
 import warnings
 from .dll_queue.queue import Queue
 
@@ -5,7 +9,7 @@ class AppState:
     def __init__(self):
         self.current_action = "Enter the name of a process or application"
         self.application_name = None
-        self.has_scanned = False
+        self.has_scanned_and_killed = False
         self.processes_scanned_count = 0
         self.unprocessed_strings_q = Queue()
         self.kill_q = Queue()
@@ -18,12 +22,10 @@ class AppState:
         """ Returns the current action so the user knows whats going on """
         return self.current_action
 
-    def set_name(self, application_name=None):   
+    def set_name(self, application_name=None):
         """ Allows us to set the application name the user wants to kill """
         # TODO: Setup logger for all the print statements below
-
-
-        # print(f"\n\n*****\nIncoming name: {application_name}, Previous name: {self.application_name}\n")        
+        # print(f"\n\n*****\nIncoming name: {application_name}, Previous name: {self.application_name}\n")
         # Case: User double clicked the kill button
         if self.application_name == application_name:
             # print("\nNameWarning @app_state.py:\n   Incoming name is the same as name in state")
@@ -33,7 +35,8 @@ class AppState:
         # Case: Trying to kill an application with a different name
         elif self.application_name != None:
             # TODO: The user May be trying to kill another application, we need to handle this somewhere else
-            print(f"\nNameWarning @app_state.py:\n    Error attempting to kill another application: '{application_name}', please close and re-open the application")
+            print(
+                f"\nNameWarning @app_state.py:\n    Error attempting to kill another application: '{application_name}', please close and re-open the application")
 
         # Case: There is no name set, so we set one
         else:
@@ -44,21 +47,21 @@ class AppState:
         """ Returns the application name for evaluation in the process string """
         return self.application_name
 
-    def set_has_scanned(self):
-        """ Sets a has_scanned to True to help determine if we have already scanned for running processes """
-        if self.has_scanned is False:
-            self.has_scanned = True
+    def set_has_scanned_and_killed(self):
+        """ Sets a has_scanned_and_killed to True to help determine if we have already scanned for running processes """
+        if self.has_scanned_and_killed is False:
+            self.has_scanned_and_killed = True
         else:
-            self.has_scanned = False
-    
-    def get_has_scanned(self):
-        """ Returns the has_scanned boolean """
-        return self.has_scanned
+            self.has_scanned_and_killed = False
+
+    def get_has_scanned_and_killed(self):
+        """ Returns the has_scanned_and_killed boolean """
+        return self.has_scanned_and_killed
 
     def increment_process_scanned_count(self):
         """ Increments processes_scanned by 1 everytime it gets called """
         self.processes_scanned_count = self.processes_scanned_count + 1
-    
+
     def get_processes_scanned_count(self):
         """ Returns the total number of processes scanned """
         return self.processes_scanned_count
@@ -76,12 +79,12 @@ class AppState:
         """ Using the Queue 'q', we are able to Remove & Return the first item from the queue """
         return self.kill_q.dequeue()
 
-    
     def _reset_state(self):
         """ This completely wipes and resets the state,
             this is useful for when the user wants to kill more than one application"""
         # TODO: Implement _reset_state
         pass
+
 
 # Creates the state object so it can be accesses from anywhere in the application
 state = AppState()
